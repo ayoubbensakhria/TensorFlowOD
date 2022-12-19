0d8e9f33406c633876f8df07aa27b81a696aa571
Ayoub <bensakhria.ayoub@gmail.com>
2022-12-19 22:09:53 +0000

    fix output getDimensions

--- 7144COMP/preprocessing/getDimensions.py
+++ 7144COMP/preprocessing/getDimensions.py
@@ -18,11 +18,24 @@ def get_dimensions(path, extension="jpg"):
     """
     # list of tuples containing heights and widths
     hw_pairs = []
+    # list of classes with annotation count
+    annotation_count = {}
     # Search recursively in subdirectories
     for jpg_file in glob.glob(path + '/**/*.{ext}'.format(ext=extension), recursive=True):
         try:
             img = imread(jpg_file)
-            height,width,color = img.shape
+            # get current folder
+            current_folder = os.path.dirname(jpg_file)
+            # check if xml with the same filename exists
+            annotated = 1 if os.path.exists(os.path.splitext(jpg_file)[0] + '.xml') else 0
+            # get height and width
+            height, width, _ = img.shape
+            # update annotation count
+            if current_folder in annotation_count:
+                current_folder[current_folder] += annotated
+            else:
+                current_folder[current_folder] = annotated
+            # check class
             if height is not None and width is not None:
                 hw_pairs.append((height,width))
             else:
@@ -30,7 +43,8 @@ def get_dimensions(path, extension="jpg"):
         except:
             print('An arror occured with the file:', jpeg_file)
             continue
-    print(hw_pairs)
+    print(hw_pairs) # <- index 0
+    print (annotation_count) # <- index 1
 
 
 def main():
